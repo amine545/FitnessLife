909a9faa80c2d02317403589601ca77459a1a6cc
Lassaad MRABET <056@Dom1.Ad.sys>
2023-12-06 18:09:11 +0100

    Description of the changes

--- src/root.js
+++ src/root.js
@@ -5,6 +5,7 @@ import Navbar from './components/navbar';
 import Sign from './sign';
 import Train from './train';
 import Footer from './components/footer'
+import Welcome from './components/welcome'
 import Kpu from './sport/kpu'
 import Lunges from './sport/lunges'
 import Plank from './sport/plank'
@@ -33,6 +34,7 @@ function root() {
                 <Route path="/squat" element={<Squat />} />
                 </Route>
             </Routes>
+            <Welcome/>
             <Footer/>
         </Router>
 
